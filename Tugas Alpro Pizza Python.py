44755c76a91f8c17c678d7b08f0396c3cd3e5799
KukuhAdji <149108364+KukuhAdji@users.noreply.github.com>
2023-10-30 11:51:54 +0700

    Add files via upload

---
+++ Tugas Alpro Pizza Python.py
@@ -0,0 +1,75 @@
+print("Halo selamat datang di Pizza Python!")
+
+#pilih topping 
+print("===============")
+print("Topping Pizza :")
+print("===============")
+print("Frank Furter")
+print("Meat Lovers")
+print("Meat Monsta") 
+print("Super Supreme")
+print("Super Supreme Chicken")
+print("===============")
+HargaTopping= 43637
+ToppingPizza=input("mau topping apa? ")
+
+#pilih crust
+print("===============")
+print("Varian Crust")
+print("Pan")
+print("StuffedCrust Cheese")
+print("StuffedCrust Sausage")
+print("Cheesy Bites")
+print("Crown Cust")
+print("===============")
+Crust=input("pilih crust apa? ")
+if Crust=="StuffedCrust Cheese":
+    HargaCrust=11818
+    Hargatotal=HargaTopping+HargaCrust
+    print("Harga pizza anda Rp.", Hargatotal)
+elif Crust=="StuffedCrust Sausage":
+    HargaCrust=9091
+    Hargatotal=HargaTopping+HargaCrust
+    print("Harga pizza anda Rp. ", Hargatotal)
+elif Crust=="Cheesy Bites":
+    HargaCrust=13636
+    Hargatotal=HargaTopping+HargaCrust
+    print("harga pizza anda Rp. ", Hargatotal)
+elif Crust=="Crown Cust":
+    HargaCrust=11818
+    Hargatotal=HargaTopping+HargaCrust
+    print("harga pizzamu Rp. ", Hargatotal)
+elif Crust=="Pan":
+    Hargatotal=HargaTopping
+    print("Harga pizzamu Rp. ", Hargatotal)
+else:
+    print("mohon maaf jenis crust mu tidak valid")
+   
+#pilih ukuran pizza
+print("===============")
+print("Personal")
+print("Regular")
+print("Large")
+print("===============")
+Ukuran=input("pilih ukuran pizza anda: ")
+if Ukuran=="Personal":
+    Hargatotal+=0
+    print("Harga pizza anda Rp. ", Hargatotal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
